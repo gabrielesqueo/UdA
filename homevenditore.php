b8b670e1f29364579219a2acb0aee35956e90c86
Gabriele <gsqueo3@gmail.com>
2022-05-17 09:25:37 +0200

    Update

--- homevenditore.php
+++ homevenditore.php
@@ -59,7 +59,7 @@
                       } 
                     echo "</table></div><button class='cssbuttons-io-button' onclick='redirect()' style='margin-top: 30px;margin-left:35%'>Metti In Affitto Un Altra Proprietà! <div class='icon'><svg height='24' width='24' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path d='M0 0h24v24H0z' fill='none'></path><path d='M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z' fill='currentColor'></path></svg> </button>";
                 } else {
-                    echo "<button class='cssbuttons-io-button' onclick='redirect()'> style='margin-top: 30px;margin-left:35%'>Metti In Affitto La Tua Prima Proprietà! <div class='icon'><svg height='24' width='24' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path d='M0 0h24v24H0z' fill='none'></path><path d='M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z' fill='currentColor'></path></svg> </button>";
+                    echo "<button class='cssbuttons-io-button' onclick='redirect()' style='margin-top: 30px;margin-left:35%'>Metti In Affitto La Tua Prima Proprietà! <div class='icon'><svg height='24' width='24' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path d='M0 0h24v24H0z' fill='none'></path><path d='M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z' fill='currentColor'></path></svg> </button>";
                 }
                 mysqli_close($conn);
                 
